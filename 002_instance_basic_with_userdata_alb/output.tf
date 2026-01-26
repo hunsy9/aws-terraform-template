a49410afea441466d0890c52147ac1443a24137b
hunsy9 <juniper0917@gmail.com>
2026-01-26 11:21:54 +0000

    feat: add output file on 002_instance_basic_with_userdata for testing

--- 002_instance_basic_with_userdata_alb/output.tf
+++ 002_instance_basic_with_userdata_alb/output.tf
@@ -1,3 +1,11 @@
 output "alb_endpoint" {
     value = aws_lb.test_alb.dns_name
 }
+
+output "tg_arn" {
+    value = aws_lb_target_group.test_alb_tg.arn
+}
+
+output "instance_id" {
+    value = aws_instance.tg_instance[*].id
+}
